065a49868ccab13c8aee9ea02eafbfdd467750a9
smshack <5432tat@naver.com>
2022-12-11 16:40:26 +0900

    version 추가

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,13 +4,20 @@ import _ from "lodash";
 import { Editor } from "@toast-ui/react-editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
 const Home = ({authState,setAuthState}) => {
-  const [version, setVersion] = useState('v1')
+  const [version, setVersion] = useState('version1')
+  const [keywordList ,setKeywordList] = useState([{keyword:"",index:0}])
+  const handleChangeVersion = (e) =>{
+    setVersion(e.target.value);
+  }
   useEffect(() => {
     console.log(authState);
   }, [authState]);
+  const plusKeyworkList = () =>{
+    setKeywordList([...keywordList, { keyword: "", index: keywordList.length -1 }]);
+  }
   return (
     <div className="wrapper">
-      <div>
+      <div className="inputbox">
         {/* 유저 리스트 선택 | 버전 선택 부분 */}
         <div className="user_version">
           <div className="userlist">
@@ -30,67 +37,160 @@ const Home = ({authState,setAuthState}) => {
             </select>
           </div>
           <div className="version">
-            <select className="form-select" name="version" id="version">
+            <select
+              className="form-select"
+              name="version"
+              id="version"
+              onChange={handleChangeVersion}
+              value={version}
+            >
               <option value={`version1`}>version1</option>
               <option value={`version2`}>version2</option>
               <option value={`version3`}>version3</option>
+              <option value={`version4`}>version4</option>
             </select>
           </div>
         </div>
 
-        <div className="version1">
-          {/* 제목 부분 */}
-          <div className="input-group mb-3">
-            <span className="input-group-text" id="keyword">
-              키워드
-            </span>
-            <input
-              type="text"
-              className="form-control"
-              id="keyword"
-              placeholder="키워드를 입력해주세요"
-              aria-describedby="keyword"
-            />
+        {version == "version1" && (
+          <div className="version1">
+            {/* 제목 부분 */}
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="keyword">
+                키워드
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                id="keyword"
+                placeholder="키워드를 입력해주세요"
+                aria-describedby="keyword"
+              />
+            </div>
+            {/* 본문 부분 */}
+            <div className="input-group contentgroup">
+              <span className="input-group-text">본문</span>
+              <textarea className="form-control" aria-label="content"></textarea>
+            </div>
+            {/* 이미지 부분 */}
+            <div className="imggroup">
+              <h3 className="h4">이미지</h3>
+              <Editor
+                placeholder="내용을 입력해주세요."
+                previewStyle="vertical" // 미리보기 스타일 지정
+                height="500px" // 에디터 창 높이
+                initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
+                toolbarItems={[
+                  // 툴바 옵션 설정
+                  ["heading", "bold", "italic", "strike"],
+                  ["hr", "quote"],
+                  ["ul", "ol", "task", "indent", "outdent"],
+                  ["table", "image", "link"],
+                  ["code", "codeblock"],
+                ]}
+              ></Editor>
+            </div>
+            {/* 동영상 부분 */}
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="keyword">
+                동영상
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                id="videourl"
+                placeholder="스크롤할 페이지 url을 입력해주세요"
+                aria-describedby="videourl"
+              />
+            </div>
           </div>
-          {/* 본문 부분 */}
-          <div class="input-group contentgroup">
-            <span class="input-group-text">본문</span>
-            <textarea class="form-control" aria-label="content"></textarea>
+        )}
+        {version == "version2" && (
+          <div className="version2">
+            {/* 제목 부분 */}
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="keyword">
+                키워드
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                id="keyword"
+                placeholder="키워드를 입력해주세요"
+                aria-describedby="keyword"
+              />
+            </div>
+            {/* 이미지 부분 */}
+            <div className="imggroup">
+              <h3 className="h4">본문</h3>
+              <Editor
+                placeholder="내용을 입력해주세요."
+                previewStyle="vertical" // 미리보기 스타일 지정
+                height="500px" // 에디터 창 높이
+                initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
+                toolbarItems={[
+                  // 툴바 옵션 설정
+                  ["heading", "bold", "italic", "strike"],
+                  ["hr", "quote"],
+                  ["ul", "ol", "task", "indent", "outdent"],
+                  ["table", "image", "link"],
+                  ["code", "codeblock"],
+                ]}
+              ></Editor>
+            </div>
           </div>
-          {/* 이미지 부분 */}
-          <div className="imggroup">
-            <h3 className="h4">이미지</h3>
-            <Editor
-              placeholder="내용을 입력해주세요."
-              previewStyle="vertical" // 미리보기 스타일 지정
-              height="500px" // 에디터 창 높이
-              initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
-              toolbarItems={[
-                // 툴바 옵션 설정
-                ["heading", "bold", "italic", "strike"],
-                ["hr", "quote"],
-                ["ul", "ol", "task", "indent", "outdent"],
-                ["table", "image", "link"],
-                ["code", "codeblock"],
-              ]}
-            ></Editor>
+        )}
+        {version == "version3" && (
+          <div className="version3">
+            {/* 제목 부분 */}
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="keyword">
+                키워드
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                id="keyword"
+                placeholder="키워드를 입력해주세요"
+                aria-describedby="keyword"
+              />
+            </div>
           </div>
-          {/* 동영상 부분 */}
-          <div className="input-group mb-3">
-            <span className="input-group-text" id="keyword">
-              동영상
-            </span>
-            <input
-              type="text"
-              className="form-control"
-              id="videourl"
-              placeholder="스크롤할 페이지 url을 입력해주세요"
-              aria-describedby="videourl"
-            />
+        )}
+        {version == "version4" && (
+          <div className="version4">
+            {/* 제목 부분 */}
+            <button className="btn btn-info " onClick={plusKeyworkList}>
+              키워드 추가
+            </button>
+            <div className="input-group mb-3">
+           {keywordList.map(item =>{
+            console.log(item);
+            return <Keywordinput setKeywordList={setKeywordList} item={item}/>;
+           })}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
+  );
+};
+
+const Keywordinput = (setKeywordList, item) => {
+  
+  return (
+    <>
+      <span className="input-group-text" id="keyword">
+        {item.index}키워드
+      </span>
+      <input
+        type="text"
+        className="form-control"
+        id="keyword"
+        placeholder="키워드를 입력해주세요"
+        aria-describedby="keyword"
+      />
+    </>
   );
 };
 
